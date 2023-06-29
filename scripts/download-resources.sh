d864625a9b97cf5dc64f06d28d9f3e9b26d1fb66
CharlesCalzia <chcalzia@yahoo.com>
2023-06-29 19:55:47 +0100

    Add installation zims

---
+++ scripts/download-resources.sh
@@ -0,0 +1,9 @@
+wget https://download.kiwix.org/zim/wikipedia/wikipedia_en_for_schools_maxi_2023-06.zim #wikipedia for schools
+wget https://download.kiwix.org/zim/wikipedia/wikipedia_en_all_mini_2023-06.zim # wikipedia mini
+wget https://download.kiwix.org/zim/videos/khan-academy-videos_en_khan-academy-ed-talks_2021-04.zim # khan academy ed talks
+wget https://download.kiwix.org/zim/ted/ted_en_playlist-talks-from-inspiring-teachers_2021-12.zim # ted talks from inspiring teachers
+wget https://download.kiwix.org/zim/videos/oer4schools_en_all_2022-09.zim # oer4schools
+wget https://download.kiwix.org/zim/ted/ted_en_playlist-the-love-of-lifelong-learning_2021-12.zim # ted talks the love of lifelong learning
+wget https://download.kiwix.org/zim/wikiversity/wikiversity_en_all_maxi_2021-03.zim # wikiversity
+
+# wget https://download.kiwix.org/zim/other/crashcourse_en_all_2023-06.zim # crash course
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,20 +1,9 @@
 # Enable SSH
 
 
-sudo apt update -y
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt-cache policy docker-ce
-sudo apt install -y docker-ce
+sudo apt-get update -y && sudo apt-get upgrade
 
 sudo apt install -y python3
 sudo apt install -y python3-pip
 
 pip install -r requirements.txt
-
-sudo ./download-default.sh
-
-cd ..
-docker compose build
-docker compose up
