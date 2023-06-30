188b0d52e1fce6aa4071abf9b636de51c8c0b1de
CharlesCalzia <chcalzia@yahoo.com>
2023-06-30 01:26:58 +0100

    Add docker to installation

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,7 @@ WORKDIR /educational_resources_technology
 COPY . /educational_resources_technology/
 RUN pip install -r requirements.txt
 
+ENV DEBUG=0
+ENV RESOURCES_IMAGES_PATH="educational_resources_technology/home/static/images/resources/"
+
 EXPOSE 8000
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,15 @@
-# Enable SSH
-
-
 sudo apt-get update -y && sudo apt-get upgrade
+
+# Download docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=armhf signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+
+
 
 sudo apt install -y python3
 sudo apt install -y python3-pip
